716f2f510258834fa8916879999a408b8da166e5
Nick Rhodes <n.g.rhodes@leeds.ac.uk>
2020-07-03 10:09:39 +0100

    Use contents instead of index as default

--- conf.py
+++ conf.py
@@ -54,3 +54,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+#Play nice with read the docs, dont use index as default file
+master_doc = 'contents'
